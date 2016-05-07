b78a098770fb61f9f77caa9efbc149eb70e89c24
jh park <jhjh550@gmail.com>
2016-05-07 17:51:35 +0900

    ui widget

--- T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
+++ T03_widget/app/src/main/java/com/example/c/t03_widget/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.c.t03_widget;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTest = (Button) findViewById(R.id.btnTest);
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "btn click", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.myCheckBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "checkbox : "+isChecked,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
