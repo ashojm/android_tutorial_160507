e10179d9a19c754664602b16a31146678f6ca494
jh park <jhjh550@gmail.com>
2016-05-07 16:57:07 +0900

    - menu

--- T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
+++ T02_menu/app/src/main/java/com/example/c/t02_menu/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.c.t02_menu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     @Override
@@ -26,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "menu selected", Toast.LENGTH_LONG).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0, 100, 0, "hello world");
+        menu.add(0,101, 0, "test context menu");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId() == 100){
+            Toast.makeText(this, "hello world selected", Toast.LENGTH_LONG).show();
+        }else if(item.getItemId() == 101){
+            Toast.makeText(this, "test context menu selected", Toast.LENGTH_LONG).show();
+        }
+        return super.onContextItemSelected(item);
     }
 }
